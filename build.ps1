89427e7b6b7b47ca48efbbd0a563479861eb5946
Roncarlos <Roncarlos@DESKTOP-US27VUH>
2023-05-29 17:28:37 +0200

    react itnegration

---
+++ build.ps1
@@ -0,0 +1,7 @@
+# Execute npm run build
+Set-Location ./react-chrome-app
+npx webpack build
+
+Set-Location ../
+
+# Pack the extension here
---
+++ react-chrome-app/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.tsx",
+    mode: "production",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: [
+                    {
+                        loader: "ts-loader",
+                        options: {
+                            compilerOptions: { noEmit: false },
+                        },
+                    },
+                ],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ["file-loader"],
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "..", "extension", "popup", "build"),
+    },
+};
